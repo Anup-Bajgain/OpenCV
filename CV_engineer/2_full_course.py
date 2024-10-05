8b1b0d37d3640a400ed0cc9e5bd9bd9979a3accf
Anup-Bajgain <118955299+Anup-Bajgain@users.noreply.github.com>
2024-10-05 18:36:55 +0545

    .

--- CV_engineer/2_full_course.py
+++ CV_engineer/2_full_course.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import os
 
+
 #reading image
 # image_link = os.path.abspath("cars.jpg")
 # # print(image_link)
@@ -8,6 +9,7 @@ import os
 # print(type(img))
 # cv.imshow("cars",img)
 # cv.waitKey(0)
+
 
 #reading videos
 # video_path = os.path.abspath('video12.mp4')
@@ -22,6 +24,7 @@ import os
 # video.release()
 # cv.destroyAllWindows()
 
+
 #resizing
 # rs = cv.resize(img, (190,1080)) #values can be anything i.e greater or lesser than original value
 # w,h,d=img.shape
@@ -32,10 +35,12 @@ import os
 # cv.imshow("cars",img)
 # cv.waitKey(0)
 
+
 #cropping
 # cropped = img[200:600,100:500]
 # cv.imshow("cropped",cropped)
 # cv.waitKey(0)
+
 
 #colourspaces
 # image_link = os.path.abspath("OIP.jpg")
@@ -52,8 +57,33 @@ import os
 # # cv.imshow("gray bird",gray)
 # cv.waitKey(0)
 
+
 #Blurring
-image_link = os.path.abspath("OIP.jpg")
+# image_link = os.path.abspath("person.jpg")
+# img = cv.imread(image_link)
+# k_size =7
+# # bl = cv.blur(img, (k_size,k_size)) #normal blur
+# # bl = cv.GaussianBlur(img,(k_size,k_size),3) 
+# bl = cv.medianBlur(img,k_size)
+# cv.imshow("not blurred",img)
+# cv.imshow("blurred",bl)
+# cv.waitKey(0)
+
+#Image threshold
+# image_link = os.path.abspath("person.jpg")
+# img = cv.imread(image_link)
+# bl=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# ret,thresh =cv.threshold(bl,100,255,cv.THRESH_BINARY)
+# cv.imshow("modified",bl)
+# cv.imshow("modified_2",thresh)
+# cv.imshow("Normal",img)
+# cv.waitKey(0)
+
+
+#edge detection
+image_link = os.path.abspath("basketball_play.jpg")
 img = cv.imread(image_link)
-blurred = cv.blur()
+edge = cv.Canny(img,10,200)
+cv.imshow("not blurred",img)
+cv.imshow("edge detection",edge)
 cv.waitKey(0)
